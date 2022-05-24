d4cec2a8efdf389aa957742cd774c5e885dea093
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-05-24 22:31:22 +0200

    fixing tests

--- src/test/scala/pt1_cats/AppTest.scala
+++ src/test/scala/pt1_cats/AppTest.scala
@@ -8,6 +8,7 @@ class AppTest extends AnyFunSuite with Matchers {
   test("test console app") {
     // given
     val files = Map("test.txt" -> "Igor".getBytes)
+    val expectedState = Map("test.txt" -> "Igor", "test.log" -> "Hello Igor!")
 
     // when
     val (state, result) = App.program
@@ -16,8 +17,11 @@ class AppTest extends AnyFunSuite with Matchers {
       .value
 
     // then
-    state shouldBe Map("test.txt" -> "Igor".getBytes, "log.txt" -> "Hello Igor!".getBytes)
+    mapValuesToString(state) shouldBe expectedState
     result shouldBe "Hello Igor!"
   }
+
+  private def mapValuesToString(map: Map[String, Array[Byte]]): Map[String, String] =
+    map.view.mapValues(new String(_, "UTF-8")).toMap
 
 }
